3ff489c8d48e0dec6d8d3eb8e461d7c0143d2535
Muhammad Rifki Hafidz <rifkihafidz07@gmail.com>
2026-02-28 16:58:23 +0700

    Bump ver app constants

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,6 +1,6 @@
 class AppConstants {
   // --- General ---
-  static const String appVersion = '1.8.4';
+  static const String appVersion = '2.0.0';
   static const String appName = 'Liftly';
   static const String defaultUserId = '1';
 
